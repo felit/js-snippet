8de7d540a5ebe11a3986bc6021a5992a46bf43b5
felit <nofelit@163.com>
2013-10-16 18:41:39 +0800

    add compatible.js to js-snippet

---
+++ compatible.js
@@ -0,0 +1,7 @@
+//TODO 测试兼容性，将数据返回给server作统计
+/**
+ * 测试内容
+ * 1、javascript内核 Ecmascript3 Ecmascript5的兼容性
+ * 2、javascript内核版本
+ * 3、BOM兼容性
+ */
---
+++ type.js
@@ -0,0 +1,2 @@
+//测试对象类型: undefined,null,Object,Number,Array
+// typeof === constructor
